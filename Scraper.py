a5652fb0280e5e973002c14165fc50b89b971ab6
bar <bar.aloni@mail.huji.ac.il>
2019-11-26 12:59:35 +0200

    documentation

--- Scraper.py
+++ Scraper.py
@@ -1,3 +1,5 @@
+
+#Imports:
 from selenium import webdriver
 from scrapy.selector import Selector
 import math
@@ -11,19 +13,29 @@ class Scraper:
     WAITING_TIME = 2
     HOURS_IN_DAY = 24.0
     KICKSTARTER_URL = 'https://www.kickstarter.com/discover/categories/technology'
+    # Creator field:
     CREATOR_XPATH = '//*[@class="type-14 bold"]/text()'
+    # Title field:
     TITLE_XPATH = '//*[@class="type-28 type-24-md soft-black mb1 project-name"]/text()'
+    # Dollars Pledged field:
     DOLLARS_PLEDGED_XPATH = '//*[@class="ksr-green-500"]/text()'
+    # Dollars goal field:
     DOLLARS_GOAL_XPATH = '//*[@class="money"]/text()'
+    # Num Bakers field:
     NUM_BACKERS_XPATH = '//*[@class="block type-16 type-28-md bold dark-grey-500"]/span/text()'
+    # Days To Go field:
     TIME_LEFT_XPATH = '//*[@class="block type-16 type-28-md bold dark-grey-500"]/text()'
     TIME_TYPE_XPATH = '//*[@class="block navy-600 type-12 type-14-md lh3-lg"]/text()'
+
+    # Find projects:
     PROJECT_LINK_XPATH = '//*[@class="clamp-5 navy-500 mb3 hover-target"]'
+    # Load More pages of projects:
     LOAD_MORE_BUTTON_XPATH = '//*[@class="bttn bttn-green bttn-medium"]'
 
     def __init__(self, amount_to_load=DEFAULT_PROJECT_AMOUNT):
         """
-        :param amount_to_load:
+        Initializes a scraper object.
+        :param amount_to_load: the number project divs to crawl.
         """
         self.__driver = webdriver.Chrome(executable_path=r'resources\chromedriver.exe')
         self.__driver.minimize_window()
@@ -33,7 +45,6 @@ class Scraper:
     def __load_all_projects_divs(self):
         """
         Clicks on the "load more" button until the page contains enough project divs.
-        :return:
         """
         clicks_required = int(math.ceil(self.DEFAULT_PROJECT_AMOUNT / self.PROJECTS_PER_PAGE))
         for i in range(clicks_required):
@@ -42,7 +53,7 @@ class Scraper:
 
     def __generate_projects_links(self):
         """
-        :return:
+        Generates the set of project links.
         """
         print("Collecting links...")
         self.__driver.get(self.KICKSTARTER_URL)
@@ -51,12 +62,18 @@ class Scraper:
 
         for element in project_divs:
             inner_html = element.get_attribute('innerHTML')
-            link = inner_html.split("\"")[1]
+            link = inner_html.split("\"")[1]  # To handle cases in which escaping is evident
             self.__links.add(link)
 
         print("Finished collecting " + str(len(self.__links)) + " links.")
 
     def __extract_days_to_go(self, html):
+        """
+        Computes the days left to the project. More specifically,  If the the project displays
+        it's time by hours, it will converts it to days.
+        :param html: the projects text (html).
+        :return: the time left foe the project to close, in days.
+        """
         time_type = Selector(text=html).xpath(self.TIME_TYPE_XPATH).get()
         time_left = Selector(text=html).xpath(self.TIME_LEFT_XPATH).get()
         if time_type.startswith('hours'):
@@ -68,6 +85,13 @@ class Scraper:
     #
 
     def __parse_project_page(self, project_link, project_id):
+        """
+        Collects all of the project page relevant data: Id, Url, Creator, Title, Text , DollarsPledged, DollarsGoal,
+        NumBackers, DaysToGo.
+        :param project_link: the project's link.
+        :param project_id: the projects id- non zero int.
+        :return: returns a set containing the page's data.
+        """
         project_data = {}
         self.__driver.get(project_link)
         time.sleep(self.WAITING_TIME)
@@ -85,6 +109,10 @@ class Scraper:
         return project_data
 
     def __create_projects_dictionary(self):
+        """
+        Creates a dictionary of all project pages and their attributes.
+        :return: returns the dictionary.
+        """
         print("creating projects dictionary...")
         data = {'projects': []}
         project_num = 1
@@ -96,6 +124,10 @@ class Scraper:
         return data
 
     def scrap(self):
+        """
+        Crawls the Kickstarter's site and outputs the data of the projects that are displayed
+        there as a JSON file.
+        """
         print("Start scraping...")
         self.__generate_projects_links()
         data = self.__create_projects_dictionary()
@@ -105,8 +137,7 @@ class Scraper:
 
     def __del__(self):
         """
-        Closes the driver.
-        :return:
+        Closes the driver. Must be called as a destructor.
         """
         self.__driver.close()
 
